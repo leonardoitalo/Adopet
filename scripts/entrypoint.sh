dc2485d65f47e823a21741d6ddb097f17ecf9b83
leonardoitalo <131911745+leonardoitalo@users.noreply.github.com>
2024-10-08 11:32:23 -0300

    chore(docker): optimizing dockerfile to run scripts from a script file and adding .dockerignore file

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Cria as migrações do banco de dados 
+python manage.py makemigrations
+
+# Aplica as migrações do banco de dados
+python manage.py migrate
+
+# Inicia o servidor Django
+python manage.py runserver 0.0.0.0:8000
