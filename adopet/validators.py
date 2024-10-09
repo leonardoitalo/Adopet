2e1176f68ca7de86f6e5cd3deb6dfcf67bf1e8b8
leonardoitalo <131911745+leonardoitalo@users.noreply.github.com>
2024-10-09 08:40:22 -0300

    feat: creating name validation to contain whitespace

--- adopet/validators.py
+++ adopet/validators.py
@@ -1,7 +1,8 @@
 import re
 
-def invalid_name(name: str):
-    return not name.isalpha()
+def invalid_name(value: str):
+    pattern = r'^[A-Za-z\s]+$'
+    return not re.match(pattern, value)
 
 def invalid_age(value):
     pattern = r'^\d+\s+(dia|dias|mes|meses|ano|anos)$'
