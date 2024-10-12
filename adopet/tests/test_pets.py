5d767371531ad0a33e184d1c371e31044999f073
leonardoitalo <131911745+leonardoitalo@users.noreply.github.com>
2024-10-12 15:45:55 -0300

    test: adding test to check if a shelter can create, delete and update a pet

--- adopet/tests/test_pets.py
+++ adopet/tests/test_pets.py
@@ -3,12 +3,14 @@ from rest_framework import status
 from adopet.tests.base_test import APIBaseTestCase
 from adopet.serializers import PetSerializer
 from adopet.models import Pet, Shelter
+from django.contrib.auth.models import User 
 
 class PetsTestCase(APIBaseTestCase):
     def setUp(self):
         super().setUp()
         self.url = reverse('Pets-list')
         self.shelter = Shelter.objects.get(pk=3)
+        self.user_shelter = User.objects.get(id=2)
         self.pet = Pet.objects.get(pk=2)
         
     def test_request_get_list_pets(self):
@@ -37,10 +39,33 @@ class PetsTestCase(APIBaseTestCase):
         }
 
         response = self.client.post(self.url, datas)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        
+    def test_check_shelter_can_post_pet(self):
+        """Teste para verificar se um abrigo consegue criar um pet"""
+        datas = {
+            'name': 'Test Post Pet',
+            'age': '1 ano',
+            'size': 'pequeno',
+            'description': 'Alegre',
+            'address': 'São Paulo (SP)',
+            'adopted': False,
+            'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQktXg5_v8-L9AslphhrFvphE12SWkGl-_Jig&usqp=CAU',
+            'shelter': self.shelter.pk
+        }
+
+        self.client.force_authenticate(user=self.user_shelter)
+        response = self.client.post(self.url, datas)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         
     def test_request_delete_pet(self):
         """Teste de requisição DELETE para um pet"""
+        response = self.client.delete(f'{self.url}{self.pet.pk}/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        
+    def test_check_shelter_can_delete_pet(self):
+        """Teste para verificar se um abrigo consegue deletar uma adoção"""
+        self.client.force_authenticate(user=self.user_shelter)
         response = self.client.delete(f'{self.url}{self.pet.pk}/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         
@@ -57,5 +82,22 @@ class PetsTestCase(APIBaseTestCase):
             'shelter': self.shelter.pk
         }
 
+        response = self.client.put(f'{self.url}{self.pet.pk}/', datas)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        
+    def test_check_shelter_can_put_pet(self):
+        """Teste para verificar se um abrigo consegue alterar um pet"""
+        datas = {
+            'name': 'Test Put Pet',
+            'age': '2 meses',
+            'size': 'médio',
+            'description': 'Carinhoso',
+            'address': 'Rio de Janeiro (RJ)',
+            'adopted': False,
+            'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQktXg5_v8-L9AslphhrFvphE12SWkGl-_Jig&usqp=CAU',
+            'shelter': self.shelter.pk
+        }
+        
+        self.client.force_authenticate(user=self.user_shelter)
         response = self.client.put(f'{self.url}{self.pet.pk}/', datas)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
