cfb9fc2bccce190a210762e82b5379c37b8cd4ca
leonardoitalo <131911745+leonardoitalo@users.noreply.github.com>
2024-10-12 13:06:25 -0300

    fix: changing permission so that only shelters are allowed to create an adoption

--- adopet/permissions.py
+++ adopet/permissions.py
@@ -4,14 +4,9 @@ class IsShelterAndCanDeleteAdoption(permissions.BasePermission):
     """
     Custom permission to allow only shelters to exclude adoptions.
     """
-
     def has_permission(self, request, view):
         # Allow all users access to view adoptions 
         if request.method in permissions.SAFE_METHODS:  # GET, HEAD, OPTIONS
-            return True
-        
-        # Allow all users to create an adoption(POST)
-        if request.method == 'POST':
             return True
         
         # The user needs to be authenticated and be a shelter
