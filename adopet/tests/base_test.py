082654e44e3eabc8895c6d8a58388a73cd9a7f4a
leonardoitalo <131911745+leonardoitalo@users.noreply.github.com>
2024-10-12 18:49:48 -0300

    test: creating tests to check the api request limit

--- adopet/tests/base_test.py
+++ adopet/tests/base_test.py
@@ -1,5 +1,6 @@
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User 
+from django.core.cache import cache
 
 class APIBaseTestCase(APITestCase):
     fixtures = ['prototype_db', 'prototype_users_db']
@@ -7,6 +8,7 @@ class APIBaseTestCase(APITestCase):
     def setUp(self):
         self.user = User.objects.create_superuser(username='admin', password='admin')
         self.client.force_authenticate(user=self.user)
+        cache.clear()
     
     def get_serialized_data(self, obj, serializer_class):
         serializer = serializer_class(obj)
--- config/settings.py
+++ config/settings.py
@@ -107,12 +107,18 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication', 
-        'rest_framework.authentication.SessionAuthentication',  
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '100/day'
+    },
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
